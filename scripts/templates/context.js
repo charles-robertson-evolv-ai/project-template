b7f90ccd67ea245c8c99a0ebf3b440d152201fb7
Charles Robertson <charles.robertson@evolv.ai>
2023-01-16 16:06:22 -0500

    Tweaks

--- scripts/templates/context.js
+++ scripts/templates/context.js
@@ -1,8 +1,10 @@
 import { rule, $, $$, $i, store, app, log } from './_imports/_setup.js';
 import instrumentPage from './_imports/_instrument.js';
-__variantImports__;
+__variantImports__
 
-rule.id = '__contextId__';
-__variantDeclarations__;
+// Uncomment the line below in production to enable SPA handling
+// rule.id = '__contextId__';
+
+__variantDeclarations__
 
 instrumentPage();
